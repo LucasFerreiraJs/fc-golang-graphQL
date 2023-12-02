b8f0875d0bb90e752adc849ebf0cc0078bb8f4ea
Lucas Ferreira <casferreiraa@gmail.com>
2023-12-02 04:01:51 -0300

    feat(category): ajust resolver and persisting category by playground

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,12 @@
 package graph
 
+import "github.com/lucasferreirajs/13-GraphQL/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// onde injetamos dependência
+type Resolver struct {
+	CategoryDB *database.Category
+}
